154f5a7e92ff65dd904a1b8d56aba55715b9d918
Khang Ly <khangly@berkeley.edu>
2019-10-11 05:31:33 -0700

    Fixed bugs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # handshook
 ## Introduction
-Handshook is a Handshake applicator. It automatically applies to easy jobs on Handshake, i.e. jobs that require only resume and/or transcript.
+Handshook is a Handshake applicator. It automatically applies to easy jobs on Handshake, i.e. jobs that require only resume or cover letter or transcript.
 
 ## Dependencies
 The script uses `requests` library.
@@ -9,16 +9,22 @@ The script uses `requests` library.
 You need to provides this program three things:
 * Your jobs search string
 * Your cookies
-* Your resume ID and transcript ID
+* Your resume ID, cover letter ID, and transcript ID
 
-You can obtain the job search string by just search normally for jobs, and copy the search url string. Please sort the jobs by latest first!
+You can obtain the job search string by just search normally for jobs, and copy the search url string. Please sort the jobs by "Date Posted"! The link looks like
+```
+https://berkeley.joinhandshake.com/postings?page=1&per_page=25&sort_direction=desc&sort_column=created_at&job.student_screen.disable_majors=true&job.student_screen.disable_school_years=true&job.student_screen.disable_graduation_date=true&job.student_screen.disable_work_auth=true&job.student_screen.disable_gpa=true&job.job_types%5B%5D=3&job.salary_types%5B%5D=1&qualified_only=false&majors%5B%5D=14484&majors%5B%5D=28
+```
 
-For cookies, follow the example in `conf.json`, copy the names and values from the domain `.joinhandshake.com` to that file.
-The reason cookies are used because I don't have access to neither the CalCentral Login API nor Handshake API.
+For cookies, follow the example in `conf.json`, copy the names and values from the domain `.joinhandshake.com` to that file. If you use Firefox, login to Handshake and go to developer mode by pressing <kbd>F12</kbd> or just by right clicking somewhere on the page and choose "Inspect Element". Then go to "Storage" tab, click on https://berkeley.joinhandshake.com and copy the cookies from there.
+The reason I use cookies is because I don't have access to neither the CalCentral Login API nor Handshake API.
 
-To get the resume ID and transcript ID just go to those documents and copy the number after `users/`.
+To get the resume ID, cover letter ID, and transcript ID go to those documents and copy the number after `documents#`. The link looks like
+```
+https://berkeley.joinhandshake.com/users/12345678/documents#12345678
+```
 
 Finally put those things in `config.json` and set `valid` to `true`. Run the script and enjoy!
 
 ## Note
-This is still in development. I'll add features such as notification via email when your cookies don't work, and make it save work better so you can just set up it as a cron job and don't worry about applying anymore!
+Let me know if anything goes wrong!
--- handshake.py
+++ handshake.py
@@ -2,7 +2,7 @@ import requests
 import json
 import datetime
 
-CONF_FILE = "conf.json"
+CONF_FILE = "conf2.json"
 JOBS_FILE = "jobs.csv"
 WAIT_FILE = "wait.json"
 LEN_CSRF = 88
@@ -11,20 +11,37 @@ ACCEPT_GET = "application/json"
 ACCEPT_POST = "application/json, text/javascript, */*; q=0.01"
 CONTENT_TYPE = "application/json; charset=utf-8"
 RESUME_TYPE_ID = 1
+COVER_TYPE_ID = 2
 TRANSCRIPT_TYPE_ID = 3
 
 
+def sanitize_url(url):
+    left = url.find("page=")
+    if left != -1:
+        right = url.find('&', left)
+        if right != -1:
+            return url[:left] + url[right + 1:]
+        return url[:left]
+    return url
+
+
 def read_conf():
-    """Read the configuration file and return Json Data, Valid, URL, Cookie, Date, Resume ID, Transcript ID"""
+    """Read the configuration file and return the dictionary representation of configs."""
     with open(CONF_FILE, "r") as conf_file:
-        data = json.load(conf_file)
-        valid = data["valid"]
-        URL = data["url"].replace("page=1&", '')
-        cookies = data["cookies"]
-        date = data["date"]
-        resume = data["resume"]
-        transcript = data["transcript"]
-        return data, valid, URL, cookies, date, resume, transcript
+        configs = json.load(conf_file)
+        configs["url"] = sanitize_url(configs["url"])
+        return configs
+
+
+def read_wait_file():
+    """Read the wait file that contains jobs which were not applied to in the previous run."""
+    try:
+        wait_file = open(WAIT_FILE, 'r')
+        waited_jobs = json.load(wait_file)
+        wait_file.close()
+    except FileNotFoundError:
+        waited_jobs = list()
+    return waited_jobs
 
 
 def get_csrf_token(session):
@@ -45,77 +62,71 @@ class Job:
         self.employer = data["employer_name"]
         self.type = data["job"]["type"]
         self.apply_type = data["job"]["job_apply_setting"]["apply_type"]
-        self.documents = [document_type["document_type_id"] for document_type in
-                          data["job"]["required_job_document_types"]]
+        self.document_type_ids = [document_type["document_type_id"] for document_type in
+                                  data["job"]["required_job_document_types"]]
 
     @classmethod
-    def set(cls, session, date, resume, transcript):
+    def set(cls, session, configs):
         """Set various attributes"""
         cls.session = session
-        cls.date = date
-        cls.resume = resume
-        cls.transcript = transcript
+        cls.date = configs["date"]
+        cls.documents = {RESUME_TYPE_ID: configs["resume"], COVER_TYPE_ID: configs["cover"],
+                         TRANSCRIPT_TYPE_ID: configs["transcript"]}
         cls.csrf_token = get_csrf_token(session)
         cls.now = datetime.datetime.utcnow().isoformat()
 
     def apply(self):
-        """Apply to this job, return 0 if succeed, return 1 if cookie errors, 2 if cover letter required,
-            3 if not opened yet, 4 if the job requires applying externally"""
+        """Apply to this job, return 0 if succeed, return 1 if cookie errors, 2 if not opened yet,
+        3 if the job requires applying externally, 4 if the job required other documents"""
         if self.apply_type != "handshake":
-            return 4
-        if self.start > self.now:
             return 3
-        if 2 in self.documents:
+        if self.start and self.start > self.now:
             return 2
         headers = {"Accept": ACCEPT_POST, "Host": HOST, "X-CSRF-Token": self.csrf_token, "Content-Type": CONTENT_TYPE}
         document_ids = list()
-        for document in self.documents:
-            if document == RESUME_TYPE_ID:
-                document_ids.append(self.resume)
-            else:
-                document_ids.append(self.transcript)
+        for document_type_id in self.document_type_ids:
+            if document_type_id not in (RESUME_TYPE_ID, COVER_TYPE_ID, TRANSCRIPT_TYPE_ID):
+                return 4
+            document_ids.append(self.documents[document_type_id])
         data = json.dumps(
             {"application": {"applicable_id": self.id, "applicable_type": self.type, "document_ids": document_ids},
              "work_authorization_status": None})
         result = self.session.post('https://berkeley.joinhandshake.com/jobs/' + str(self.id) + "/applications",
                                    headers=headers, data=data)
         if result.status_code != requests.codes.ok:
-            print(result.text)
             return 1
+        print("Applied to %s successfully!" % self.name)
         return 0
 
-    def wait(self, waitlist):
-        """Add to WAITLIST"""
-        waitlist.append(self.data)
+    def wait(self, wait_list):
+        """Add to WAIT_LIST"""
+        wait_list.append(self.data)
 
     def write(self, jobs_file):
         """Write job info to JOBS_FILE"""
-        jobs_file.write(str(self.id) + ", \"" + self.name + "\", \"" + self.employer + "\", " + self.now + '\n')
+        jobs_file.write("%d, \"%s\", \"%s\", \"%s\"\n" % (self.id, self.name, self.employer, self.now))
 
 
-def process():
-    configs, valid, URL, cookie, date, resume, transcript = read_conf()
+def main():
+    configs = read_conf()
     cookie_error = False
-
-    if not valid:
-        print("Set the conf.json file or update your cookie")
-        return
+    if not configs["valid"]:
+        print("RTFM!!!")
+        exit(1)
 
     session = requests.Session()
-    session.cookies.update(cookie)
+    session.cookies.update(configs["cookies"])
+    Job.set(session, configs)
 
-    Job.set(session, date, resume, transcript)
-    with open(WAIT_FILE, "r") as wait_file:
-        original_waitlist = json.load(wait_file)
-    waitlist = list()
-    jobs_file = open(JOBS_FILE, 'a')
-
-    for job_data in original_waitlist:
+    waited_list = read_wait_file()
+    wait_list = list()
+    jobs_file = open(JOBS_FILE, 'a+')
+    for job_data in waited_list:
         job = Job(job_data)
         ret = job.apply()
         if ret != 0:
-            if ret == 3:
-                job.wait(waitlist)
+            if ret == 2:
+                job.wait(wait_list)
             elif ret == 1:
                 cookie_error = True
                 break
@@ -123,16 +134,19 @@ def process():
             job.write(jobs_file)
 
     page = 1
-    while not cookie_error:
-        jobs = session.get(URL + "&page=" + str(page), headers={"Host": HOST, "Accept": ACCEPT_GET}).json()
+    see_old_jobs = False
+    date = datetime.datetime.utcnow().isoformat()
+    while not cookie_error and not see_old_jobs:
+        jobs = session.get(configs["url"] + "&page=" + str(page), headers={"Host": HOST, "Accept": ACCEPT_GET}).json()
         for job_data in jobs["results"]:
             job = Job(job_data)
-            if date > job.date:
+            if configs["date"] > job.date:
+                see_old_jobs = True
                 break
             ret = job.apply()
             if ret != 0:
-                if ret == 3:
-                    job.wait(waitlist)
+                if ret == 2:
+                    job.wait(wait_list)
                 elif ret == 1:
                     cookie_error = True
                     break
@@ -140,13 +154,18 @@ def process():
                 job.write(jobs_file)
         page += 1
 
-    configs["valid"] = cookie_error
+    configs["valid"] = not cookie_error
     configs["cookies"] = requests.utils.dict_from_cookiejar(session.cookies)
-    with open(WAIT_FILE, 'w') as wait_file:
-        json.dump(wait_file, waitlist)
+    if cookie_error:
+        print("Cookies are not valid, please provide new ones!!!")
+    else:
+        configs["date"] = date
+        with open(WAIT_FILE, 'w') as wait_file:
+            json.dump(wait_list, wait_file)
     with open(CONF_FILE, 'w') as conf_file:
         json.dump(configs, conf_file)
     jobs_file.close()
 
 
-process()
+if __name__ == '__main__':
+    main()
